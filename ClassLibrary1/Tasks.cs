0fff4b5bf7de4bf3e3b71bb7daf5032e293ca432
Vitalii Samchynskyi <samuelvit88@gmail.com>
2025-12-14 23:03:14 +0200

    Add project files.

---
+++ ClassLibrary1/Tasks.cs
@@ -0,0 +1,67 @@
+﻿namespace ClassLibrary1
+{
+    //First part - Employee
+    public class Employee
+    {
+        private decimal salary;
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Salary can't be negative");
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+        public Employee(decimal salary) 
+        { 
+            Salary = salary;
+        }
+        public static Employee operator +(Employee e, decimal amount)
+        {
+            return new Employee(e.Salary + amount);
+        }
+        public static Employee operator -(Employee e, decimal amount)
+        {
+            return new Employee(e.Salary - amount);
+        }
+        public static bool operator ==(Employee e1, Employee e2) 
+        {
+            return e1.Salary == e2.Salary;
+        }
+        public static bool operator !=(Employee e1, Employee e2)
+        {
+            return !(e1 == e2);
+        }
+        public static bool operator <(Employee e1, Employee e2)
+        {
+            return e1.Salary < e2.Salary;
+        }
+        public static bool operator >(Employee e1, Employee e2)
+        {
+            return e1.Salary > e2.Salary;
+        }
+        //Is it needed (rows 55-65)? Generated because of C# suggestion
+        public override bool Equals(object? obj)
+        {
+            return obj is Employee employee &&
+                   salary == employee.salary &&
+                   Salary == employee.Salary;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(salary, Salary);
+        } 
+    }
+}
---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary1;
+
+namespace OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First part - Employee
+            Employee e1 = new Employee(2000);
+            Employee e2 = new Employee(3000);
+
+            e1 = e1 + 2000;
+            e2 = e2 - 1000;
+
+            Console.WriteLine(e1.Salary);
+            Console.WriteLine(e2.Salary);
+
+            Console.WriteLine(e1 > e2);
+            Console.WriteLine(e1 == e2);
+        }
+    }
+}
